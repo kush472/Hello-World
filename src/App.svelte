8f3b4ac58543551640d23e31e0a0035da60c1072
kush <kushp921@gmail.com>
2023-09-13 14:59:52 -0700

    Updated version of Svelte template

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,11 @@
 
 <main>
 	<h1>Hello {name}!</h1>
+	<b> This is an extra line of text that was not in the template before,</b>
+	<c> Although I don't really know how it works I just copied the other code with text to create these</c>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<d> Trying to figure out how to start text on a new line on the page because sometimes it works and sometimes it doesn't</d>
+	<e> This version has been uploaded to Git</e>
 </main>
 
 <style>
